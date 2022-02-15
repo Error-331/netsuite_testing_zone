7408966cabe2e6439444ccad9ac31527f3687eaf
Error-331 <red331@mail.ru>
2022-02-15 03:25:33 +0200

    Add 'values' param.

--- src/Utilities/tests/search/filter.js
+++ src/Utilities/tests/search/filter.js
@@ -2,6 +2,7 @@ function constructorMockImplementation(options) {
     this.name = options.name ?? null;
     this.join = options.join ?? null;
     this.operator = options.operator ?? null;
+    this.values = options.values ?? null;
     this.summary = options.summary ?? null;
     this.formula = options.formula ?? null;
 }
