dcfb6a86a3e7323a807cc1be3a5a24d658129678
Error-331 <red331@mail.ru>
2022-02-16 22:39:16 +0200

    Add suite billing utils test mocks.

---
+++ src/Utilities/tests/utilities/bs_cm suite_billing_settings_utils.js
@@ -0,0 +1,11 @@
+function initSuiteBillingBSNEnvSettingsMockImplementation() {
+    return {
+        bsn1yrItemNum: 849,
+        bsnc1yrItemNum: 850,
+        bsnee1yrItemNum: 884,
+    }
+}
+
+export {
+    initSuiteBillingBSNEnvSettingsMockImplementation,
+}
