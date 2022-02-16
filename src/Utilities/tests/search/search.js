3822e40af10c2aa491ff6e50d3240c4155743c64
Error-331 <red331@mail.ru>
2022-02-17 01:15:23 +0200

    Add createColumnMockImplementation() function.

--- src/Utilities/tests/search/search.js
+++ src/Utilities/tests/search/search.js
@@ -1,9 +1,15 @@
 import Filter from 'N/search/filter';
+import Column from 'N/search/column';
 
 function createFilterMockImplementation(options) {
     return new Filter.constructor(options);
 }
 
+function createColumnMockImplementation(options) {
+    return new Column.constructor(options);
+}
+
 export {
-    createFilterMockImplementation
+    createFilterMockImplementation,
+    createColumnMockImplementation,
 }
