ad332fd8c3096b123a30ef06a80844a63f0ea514
Error-331 <red331@mail.ru>
2022-02-17 01:16:07 +0200

    Add tests for addColumn() function.

--- __tests__/SuiteScripts/sergei_s_s/custom_modules/utilities/bs_cm_search_utils-test.js
+++ __tests__/SuiteScripts/sergei_s_s/custom_modules/utilities/bs_cm_search_utils-test.js
@@ -1,24 +1,31 @@
 import search from 'N/search';
 import Filter from 'N/search/filter';
+import Column from 'N/search/column';
 
 import {
     findIdxInObjectsArrayByKeyValue,
+
     addFilter,
-    addFilter1,
+    addColumn,
 } from './../../../../../src/FileCabinet/SuiteScripts/sergei_s_s/custom_modules/utilities/bs_cm_search_utils';
 
-import { constructorMockImplementation } from './../../../../../src/Utilities/tests/search/filter';
-import { createFilterMockImplementation } from './../../../../../src/Utilities/tests/search/search';
+import { constructorMockImplementation as filterConstructorMockImplementation  } from './../../../../../src/Utilities/tests/search/filter'
+import { constructorMockImplementation as columnConstructorMockImplementation } from './../../../../../src/Utilities/tests/search/column';
+import { createFilterMockImplementation, createColumnMockImplementation } from './../../../../../src/Utilities/tests/search/search';
 
 jest.mock('N/search');
 jest.mock('N/search/filter');
+jest.mock('N/search/column');
 
 beforeEach(() => {
     jest.clearAllMocks();
     jest.resetAllMocks();
 
-    Filter.constructor.mockImplementation(constructorMockImplementation);
+    Filter.constructor.mockImplementation(filterConstructorMockImplementation);
+    Column.constructor.mockImplementation(columnConstructorMockImplementation);
+
     search.createFilter.mockImplementation(createFilterMockImplementation);
+    search.createColumn.mockImplementation(createColumnMockImplementation);
 });
 
 const testArray1 = [
@@ -88,6 +95,44 @@ describe('Custom modules / utilities / search utilities tests', () => {
             testFilter2,
             testFilter3,
         ]);
+    });
 
+    it('it should properly concat column objects into one array', () => {
+        const testColumn1 = search.createColumn({
+            name: 'test_name1',
+            join: null,
+            label: 'test_label1',
+            summary: 'test_summary1',
+            formula: null,
+            sort: null,
+        });
+
+        const testColumn2 = search.createColumn({
+            name: 'test_name2',
+            join: null,
+            label: 'test_label2',
+            summary: 'test_summary2',
+            formula: null,
+            sort: null,
+        });
+
+        const testColumn3 = search.createColumn({
+            name: 'test_name3',
+            join: null,
+            label: 'test_label3',
+            summary: 'test_summary3',
+            formula: null,
+            sort: null,
+        });
+
+        let testColumns = [testColumn1];
+        testColumns = addColumn(testColumns, testColumn2)
+        testColumns = addColumn(testColumns, testColumn3)
+
+        expect(testColumns).toStrictEqual([
+            testColumn1,
+            testColumn2,
+            testColumn3,
+        ]);
     });
 });
