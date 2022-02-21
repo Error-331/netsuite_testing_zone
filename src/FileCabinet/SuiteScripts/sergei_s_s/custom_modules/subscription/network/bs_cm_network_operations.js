4b27aae3502dfbacd787092d51ed32c655ab626e
Error-331 <red331@mail.ru>
2022-02-21 04:36:40 +0200

    Add network operations.

--- src/FileCabinet/SuiteScripts/sergei_s_s/custom_modules/subscription/network/bs_cm_network_operations.js
+++ src/FileCabinet/SuiteScripts/sergei_s_s/custom_modules/subscription/network/bs_cm_network_operations.js
@@ -2,12 +2,16 @@
  * @NApiVersion 2.1
  */
 define([
-    './bs_cm_network_soap'
+        './../../utilities/bs_cm_general_utils',
+        './../../utilities/bs_cm_math_utils',
+        './bs_cm_network_soap'
     ],
     (
-        { soapUpdateNetworkBSNC }
+        { logExecution, isArray, isNullOrEmpty },
+        { digitsCount },
+        { soapUpdateNetworkBSNC, soapGetDeviceSubscriptions, soapDeleteDeviceSubscriptions }
     ) => {
-        function networkSuspend( networkId, suspend ){
+        function networkSuspend( networkId, suspend ) {
             const networkInfo = soapGetNetworkByIdBSNC(networkId, false);
 
             if( !networkInfo.IsError ) {
@@ -27,7 +31,54 @@ define([
             return false;
         }
 
+        function networkEmpty(netId, subId){
+            const prevSubsId = [];
+            let prevSubs = [];
+
+            const maxlength = 5;// : 6;
+            let subRecordText = '00000';// : '000000';
+            const idLength = digitsCount(subId);
+
+            subRecordText = subRecordText.substr(0, maxlength - idLength) + subId;
+
+            const filter = `[DeviceSubscription].[Network].[Id] IS ${netId} AND ([DeviceSubscription].[InvoiceNumber] IS IN ('${subRecordText}'))`;
+            const sort = '[DeviceSubscription].[Device].[Serial] DESC';
+
+            logExecution('DEBUG', 'Filter Get Subs', filter);
+            prevSubs = soapGetDeviceSubscriptions(filter, sort);
+
+            if(!isNullOrEmpty(prevSubs.error)){
+                logExecution( 'ERROR', 'Get Subs Error', prevSubs.error )
+            } else {
+                if (isArray(prevSubs.subscriptions) && prevSubs.subscriptions.length > 0) {
+                    for (let i = 0; i < prevSubs.subscriptions.length; i++) {
+                        prevSubsId.push(prevSubs.subscriptions[i].Id);
+                    }
+
+                    logExecution('DEBUG', 'prevSubsId.length ', prevSubsId.length);
+                    logExecution('DEBUG', 'prevSubsId ', JSON.stringify(prevSubsId));
+                }
+            }
+
+            const errors = [];
+            let delResult = false;
+
+            if (!isNullOrEmpty(netId)) {
+                delResult = soapDeleteDeviceSubscriptions(prevSubsId, subId);
+            }
+            if(isNullOrEmpty(delResult.error)){
+                logExecution('DEBUG', 'Deleted Subs ', delResult.deleted);
+                return true;
+            } else {
+                errors.push(delResult.error);
+                logExecution('ERROR', 'Deleted Subs ', delResult.error);
+            }
+
+            return false;
+        }
+
         return {
             networkSuspend,
+            networkEmpty,
         }
     });
