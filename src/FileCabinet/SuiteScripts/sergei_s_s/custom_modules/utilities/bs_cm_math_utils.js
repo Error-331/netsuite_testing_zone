5b89363921d18d44cad703215b11124e1e9ff719
serge <sergei.error.331@gmail.com>
2022-04-19 03:37:58 +0300

    Add more math functions.

--- src/FileCabinet/SuiteScripts/sergei_s_s/custom_modules/utilities/bs_cm_math_utils.js
+++ src/FileCabinet/SuiteScripts/sergei_s_s/custom_modules/utilities/bs_cm_math_utils.js
@@ -18,7 +18,41 @@ define([],
             return count;
         }
 
+        function eq(value1, value2) {
+            return value1 === value2;
+        }
+
+        function lt(value, boundary) {
+            return value < boundary;
+        }
+
+        function gt(value, boundary) {
+            return value > boundary;
+        }
+
+        function lte(value, boundary) {
+            return value <= boundary;
+        }
+
+        function gte(value, boundary) {
+            return value >= boundary;
+        }
+
+        function between(leftBoundary, rightBoundary, value) {
+            return (gt(value, leftBoundary) && lt(value, rightBoundary)) ||
+                eq(value, leftBoundary) ||
+                eq (value, rightBoundary);
+        }
+
+
+
         return {
             digitsCount,
+            eq,
+            lt,
+            gt,
+            lte,
+            gte,
+            between,
         }
     });
