0db20bef08821afb4b19b6ebae48873b629920a8
serge <sergei.error.331@gmail.com>
2022-05-05 04:28:56 +0300

    Add a bunch of functions.

--- src/FileCabinet/SuiteScripts/sergei_s_s/custom_modules/utilities/bs_cm_runtime_utils.js
+++ src/FileCabinet/SuiteScripts/sergei_s_s/custom_modules/utilities/bs_cm_runtime_utils.js
@@ -3,6 +3,7 @@
  */
 define([
         'N/runtime',
+        'N/url',
         './bs_cm_general_utils'
     ],
     /**
@@ -10,11 +11,37 @@ define([
  */
     (
         runtime,
+        url,
         { logExecution }
     ) => {
         function getScriptParameterByName(name) {
             const scriptObj = runtime.getCurrentScript();
             return scriptObj.getParameter({ name });
+        }
+
+        function getCurrentEmployeeId() {
+            const { id } = runtime.getCurrentUser();
+            return id;
+        }
+
+        function getScriptCurrentURLPathQuery() {
+            const currentScript = runtime.getCurrentScript();
+            return url.resolveScript({
+                scriptId: currentScript.id,
+                deploymentId: currentScript.deploymentId,
+                returnExternalURL: true
+            });
+        }
+
+        function getScriptCurrentURLPath() {
+            const currentQueryPath = getScriptCurrentURLPathQuery();
+            const sepPosition = currentQueryPath.indexOf('?');
+
+            if (sepPosition !== -1) {
+                return currentQueryPath.substr(0, sepPosition);
+            } else {
+                return currentQueryPath;
+            }
         }
 
         function printCurrentScriptRemainingUsage() {
@@ -24,6 +51,9 @@ define([
 
         return {
             getScriptParameterByName,
+            getCurrentEmployeeId,
+            getScriptCurrentURLPathQuery,
+            getScriptCurrentURLPath,
             printCurrentScriptRemainingUsage,
         }
     });
