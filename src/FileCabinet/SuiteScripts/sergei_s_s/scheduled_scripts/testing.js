c7b854cbfe1a11aa11a4ca58560f9f9bf20bd8c5
serge <sergei.error.331@gmail.com>
2022-06-23 08:57:39 +0300

    Work in progress.

--- src/FileCabinet/SuiteScripts/sergei_s_s/scheduled_scripts/testing.js
+++ src/FileCabinet/SuiteScripts/sergei_s_s/scheduled_scripts/testing.js
@@ -39,64 +39,159 @@ require([
 
 
 
-            log.debug(loadDispositionActionNameById('2'));
 
-//             const formattedDateString = format.format({ value: currentDate, type: format.Type.DATE });
 
             //   AND ROWNUM <= 50 // 143898
 
-            // 142375
-            // 123923
 
-      /*     const suiteQLQuery = `
-SELECT * FROM note WHERE ID=1461
+           const suiteQLQuery1 = `
+            SELECT customer, LISTAGG(id, ',') WITHIN GROUP (ORDER BY id)
+            FROM Subscription      
+            GROUP BY customer      
             `;
 
-//569
+           const suiteQLQuery2 = `
+ 
+           `;
 
-           log.debug('pupu'); //1446 1447
-            log.debug(query.runSuiteQL({ query: suiteQLQuery }).asMappedResults())*/
-/*
+            const suiteQLQuery = `
+                SELECT
+                    NetworkIds.custrecord_sub_network_id,
+                    
+                    GroupedSubscriptionIds.subscriptionids,
+                    GroupedSubscriptionIds.networknames,
+                    GroupedSubscriptionIds.expdates,
+                    GroupedSubscriptionIds.renewalemaildates,
+                    
+                    NetworkDisposition.id AS custrecord_id,
+                    NetworkDisposition.custrecordaction,
+                    NetworkDisposition.custrecordemployee_id,
+                    NetworkDisposition.custrecordnote,
+                    NetworkDisposition.custrecorddate_modified,
+                    NetworkDisposition.custrecorddate_add,
+                    
+                    DispositionList.name AS actionName,
+                    LastEmployee.entityid AS employeename
+                    
+                FROM 
+                    (
+                        SELECT
+                            Subscription.custrecord_sub_network_id     
+                        FROM 
+                            Subscription                   
+                        WHERE
+                            Subscription.custrecord_sub_network_id IS NOT NULL
+                        AND
+                            Subscription.custrecord_sub_network_name IS NOT NULL
+                        AND                    
+                            (
+                                    Subscription.billingsubscriptionstatus != 'TERMINATED'
+                                AND
+                                    (
+                                        (
+                                                Subscription.startdate < CURRENT_DATE 
+                                            AND
+                                                Subscription.enddate >= CURRENT_DATE 
+                                            AND
+                                                Subscription.billingsubscriptionstatus != 'TERMINATED'
+                                            AND
+                                                Subscription.enddate < ADD_MONTHS(CURRENT_DATE, 12)
+                                        ) 
+                                OR
+                                        (
+                                                Subscription.startdate >= CURRENT_DATE 
+                                            AND
+                                                Subscription.billingsubscriptionstatus = 'PENDING_ACTIVATION' 
+                                            AND
+                                                Subscription.startdate < ADD_MONTHS(CURRENT_DATE, 12)
+                                        )
+                                    )     
+                            )    
+                        GROUP BY
+                            Subscription.custrecord_sub_network_id
+                        ORDER BY
+                            Subscription.custrecord_sub_network_id 
+                    )
+                AS NetworkIds
+                
+                INNER JOIN
+                    (
+                        SELECT 
+                            custrecord_sub_network_id, 
+                            LISTAGG(FilteredPreparedSubscription.id, ',') WITHIN GROUP (ORDER BY id) AS subscriptionids,
+                            LISTAGG(FilteredPreparedSubscription.custrecord_sub_network_name, ',') WITHIN GROUP (ORDER BY FilteredPreparedSubscription.id) AS networknames,
+                            LISTAGG(FilteredPreparedSubscription.expdate, ',') WITHIN GROUP (ORDER BY FilteredPreparedSubscription.id) AS expdates,
+                            LISTAGG(FilteredPreparedSubscription.renewalemaildate, ',') WITHIN GROUP (ORDER BY FilteredPreparedSubscription.id) AS renewalemaildates
+                        FROM 
+                            (
+                                SELECT 
+                                    id,
+                                    custrecord_sub_network_id,
+                                    custrecord_sub_network_name,
+                                CASE
+                                    WHEN Subscription.startdate < CURRENT_DATE THEN Subscription.enddate
+                                    WHEN Subscription.startdate >= CURRENT_DATE THEN Subscription.startdate - 1
+                                END AS expdate,
+                
+                                CASE
+                                    WHEN Subscription.startdate < CURRENT_DATE THEN Subscription.enddate - 74
+                                    WHEN Subscription.startdate >= CURRENT_DATE THEN Subscription.startdate - 75
+                                END AS renewalemaildate
+                    
+                                FROM
+                                    Subscription 
+                                WHERE
+                                    custrecord_sub_network_id IS NOT NULL
+                                AND                    
+                                    (
+                                            Subscription.billingsubscriptionstatus != 'TERMINATED'
+                                        AND
+                                            (
+                                                (
+                                                        Subscription.startdate < CURRENT_DATE 
+                                                    AND
+                                                        Subscription.enddate >= CURRENT_DATE 
+                                                    AND
+                                                        Subscription.billingsubscriptionstatus != 'TERMINATED'
+                                                    AND
+                                                        Subscription.enddate < ADD_MONTHS(CURRENT_DATE, 12)
+                                                ) 
+                                            OR
+                                                (
+                                                        Subscription.startdate >= CURRENT_DATE 
+                                                    AND
+                                                        Subscription.billingsubscriptionstatus = 'PENDING_ACTIVATION' 
+                                                    AND
+                                                        Subscription.startdate < ADD_MONTHS(CURRENT_DATE, 12)
+                                                )
+                                            )     
+                                    ) 
+                            ) AS FilteredPreparedSubscription
+                        GROUP BY 
+                            FilteredPreparedSubscription.custrecord_sub_network_id  
+                    ) AS GroupedSubscriptionIds
+                ON
+                    (NetworkIds.custrecord_sub_network_id = GroupedSubscriptionIds.custrecord_sub_network_id)
+                LEFT OUTER JOIN
+                    customrecordbs_cr_expired_network_dispos AS NetworkDisposition
+                ON
+                    (NetworkIds.custrecord_sub_network_id = NetworkDisposition.custrecordnetwork_id)
+                LEFT OUTER JOIN
+                    customlistbs_cl_disposition_action AS DispositionList
+                ON
+                    (NetworkDisposition.custrecordaction = DispositionList.id)
+                LEFT OUTER JOIN
+                    employee AS LastEmployee
+                ON
+                    (NetworkDisposition.custrecordemployee_id = LastEmployee.id)    
+            `;
 
-            const objRecord = record.load({
-                type: 'note',
-                id: 1459,
-                isDynamic: false,
-            });
-
-            log.debug(objRecord);*/
-
-
-          /*  const ss1 = search.create({
-                type: 'note',
-                filters: [
-                    search.createFilter({name: 'recordtype', operator: search.Operator.EQUALTO, values: '569'}),
-                ],
-                columns: [
-                    search.createColumn({name: 'recordtype'}),
-                ]
-            });*/
-
-
-
-
-            /*
-                        const c = search.load({
-                            type: 'charge',
-                            id: 'customsearch_sb_renewal_charge_uni'
-                        });
-
-                        log.debug('filters', JSON.stringify(c.filters))*/
-
-            /*
-
-            filters: [{"name":"formulanumeric","operator":"equalto","values":["1"],"formula":"CASE WHEN {priceplan.startdate}={subscription.startdate} THEN 1 ELSE 0 END ","isor":false,"isnot":false,"leftparens":0,"rightparens":0},{"name":"custrecord_bs_is_import","join":"subscription","operator":"is","values":["T"],"isor":true,"isnot":false,"leftparens":1,"rightparens":0},{"name":"parentsubscriptionid","join":"subscription","operator":"isnotempty","values":[],"isor":false,"isnot":false,"leftparens":0,"rightparens":1}]
-             */
-
-          /*  const subscriptionSearch = search.load({
-                id: 102824,
-                type: search.Type.SUBSCRIPTION
-            });*/
+const res = query.runSuiteQL({ query: suiteQLQuery }).asMappedResults();
+            log.debug(res.length); // 2899 // 2938 // 2704
+            //const bb = res.map(c => c.custrecord_sub_network_id);
+log.debug(res);
+            //log.debug(bb.filter((item, index) => bb.indexOf(item) !== index))
+// ["15547","14122","13913","15226","2345","9389","12562","6091","13788","5244","15533","13437","9919","13050","12168","12297","10102","8333","13400","8493","9730","12161","15050","11741","14534","10043","15684","7402","15554","15637","14884","16105","8933","2720","13790","13900","8258","6657","12844"]
 
 
 
